87eec9d78474728b1de8dc83f9b84b2bff32c9a4
Nareg <nareg@Nathans-MacBook-Pro.local>
2019-10-14 15:09:25 -0400

    html changes

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -5,7 +5,7 @@ export default () => {
   return (
     <div>
       Im some other page
-      <Link to="/">Go back to home page!</Link>
+      <Link to="/"> Go back to home page! </Link>
     </div>
   );
 };
