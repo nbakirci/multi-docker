338f1b96b43ae3f8fd26da0887e8a06032e80023
Nareg <nareg@Nathans-MacBook-Pro.local>
2019-10-17 01:06:20 -0400

    fix calc on beanstalk

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Hallo - Fibonacci Calculator</h1>
-            <Link to="/">Home</Link> <br />
+            <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
--- client/src/Fib.js
+++ client/src/Fib.js
@@ -31,8 +31,6 @@ class Fib extends Component {
     await axios.post('/api/values', {
       index: this.state.index
     });
-    this.fetchValues();
-    this.fetchIndexes();
     this.setState({ index: '' });
   };
 
