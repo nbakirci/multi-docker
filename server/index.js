45be04d16a37e4c4a62d5f3dc8dc11385b78f8a7
Nareg <nareg@Nathans-MacBook-Pro.local>
2019-10-17 23:05:01 -0400

    fix calc on beanstalk

--- server/index.js
+++ server/index.js
@@ -37,16 +37,25 @@ app.get('/', (req, res) => {
   res.send('Hi');
 });
 
-app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * from values');
-
-  res.send(values.rows);
+app.get('/values/all', async (req, res, next) => {
+  try {
+    const values = await pgClient.query('SELECT * from values');
+    res.send(values.rows);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 });
 
-app.get('/values/current', async (req, res) => {
-  redisClient.hgetall('values', (err, values) => {
-    res.send(values);
-  });
+app.get('/values/current', async (req, res, next) => {
+  try {
+    redisClient.hgetall('values', (err, values) => {
+      res.send(values);
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 });
 
 app.post('/values', async (req, res) => {
